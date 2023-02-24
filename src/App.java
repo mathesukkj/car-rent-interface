49c4d507a2c07e2d98927746e81820617789b2a0
mathesukkj <kemuel.g7363@gmail.com>
2023-02-23 21:11:21 -0300

    feat: add basic logic

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import entities.CarRental;
+import entities.Vehicle;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the rent data ");
+        System.out.print("Car model: ");
+        Vehicle vehicle = new Vehicle(s.nextLine());
+        System.out.print("Removal (dd/MM/yyyy HH:mm): ");
+        LocalDateTime start = LocalDateTime.parse(s.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        System.out.print("Return (dd/MM/yyyy HH:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(s.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        System.out.print("Price per hour: ");
+        double pricePerHour = s.nextDouble();
+        System.out.print("Price per day: ");
+        double pricePerDay = s.nextDouble();
+
+        CarRental rent = new CarRental(start, finish, vehicle);
+
+        s.close();
     }
 }
